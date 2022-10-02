1183a9322fd2bfd3ac41956942900713d09c4bfb
BekaBitarashvili <bekabitarashvili@gmail.com>
2022-10-02 22:49:42 +0400

    Add Attributes

--- BasicCalculator/Program.cs
+++ BasicCalculator/Program.cs
@@ -2,6 +2,36 @@
 {
     private static void Main(string[] args)
     {
-        
+        Console.WriteLine("Simple Calculator");
+        Console.WriteLine("----------");
+        Console.WriteLine("Please Enter First Number: ");
+        int firstNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("Please Enter Second Number: ");
+        int secondNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("Choose Operator: +,-,*,/");
+        string operator1 = (Console.ReadLine());
+
+        if (operator1 == "+")
+        {
+            Console.WriteLine($"Answer is : {firstNumber + secondNumber}");
+        }
+        else if (operator1 == "-")
+        {
+            Console.WriteLine($"Answer is : {firstNumber - secondNumber}");
+        }
+        else if (operator1 == "*")
+        {
+            Console.WriteLine($"Answer is : {firstNumber * secondNumber}");
+        }
+        else if (operator1 == "/")
+        {
+            Console.WriteLine($"Answer is : {firstNumber / secondNumber}");
+        }
+        else
+        {
+            Console.WriteLine("ERROR");
+        }
+
+
     }
 }
